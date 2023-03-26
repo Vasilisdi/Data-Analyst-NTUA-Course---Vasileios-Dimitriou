aadd80d3e09be422e2bacfcee213c20e5ada9b7b
Vasileios Dimitriou <dimitriouavasileios@gmail.com>
2023-03-26 13:30:38 +0300

    c1-u3
    Presentation assignment

--- Final_Assignment.R
+++ Final_Assignment.R
@@ -45,28 +45,26 @@ lines(1:10, totwins)
 #benigh (2) or malignant (4) / first or second
 #So c the hclust model considers all the other data except for Class domain
 #and we can notice that it assigns them as supposed into 2 dif clusters
-groups <- cutree(c,k=2)
-table(dat$Class,groups)
 
 #Considering the NbClust Result
 #Hierarchical clustering
 plot(c)
 rect.hclust(c,k=2)
 groups <- cutree(c,k=2)
-table(dat$Class,groups)
+table(Actual_state = dat$Class,hier_clust_model = groups)
 
 #k-means clustering
 k_cl=kmeans(pmatrix,2)
 
 #Comparison of kmeans with real class cases
-table(dat$Class,k_cl$cluster)
+table(Actual_state = dat$Class, model_pred = k_cl$cluster)
 view(cbind(dat$Class,2*k_cl$cluster))
 
 #Comparison of kmeans and Hclust with 2 clusters
 #We can notice that they have slight differences
 #kmeans (12 and 20 off) / hclust (9 and 20 off).
 #We can check the similarity to each other too
-table(groups,k_cl$cluster)
+table( hier_clust_model = groups, kmeans_model = k_cl$cluster)
 
 
 #########################################################
@@ -76,14 +74,15 @@ table(groups,k_cl$cluster)
 #info that we have from the other columns
 
 
-#Turn Class into categorical var
 #Before I make use of this I store it
 #in another value so as to make use of it later on
-datClass=dat$Class
+#datClass=dat$Class
+#Turn Class into categorical var - factor
+datClass = dat$Class
 dat$Class <- as.factor(dat$Class)
 str(dat)
 
-tree_model <- rpart(Class~ . , data=dat[1:599,2:11] , method="anova")
+tree_model <- rpart(Class~ . , data=dat[1:599,2:11])
 summary(tree_model)
 plot(tree_model)
 text(tree_model)
@@ -91,13 +90,16 @@ fancyRpartPlot(tree_model)
 
 #check tree model
 #Predicting some Class values and checking them
-res <- predict(tree_model,dat[600:699,2:10])
+res <- predict(tree_model,dat[600:699,2:10],type = "class")
 
 #Processing the p float value in order to make it being able to take 2 categorical values
-res1 = c()
-for (i in 1:length(res)){res1[i]=round(res[i], digits = 0)}
-view(cbind(2*as.numeric(dat[600:699,11]),2*res1))
-table(2*as.numeric(dat[600:699,11]),res1)
+view( cbind(Actual_state =2*as.numeric(dat[600:699,11]),Pred = 2*as.numeric(res)))
+table(Actual_state = dat[600:699,11],Pred = res)
+table( hier_clust_model = groups[600:699], Tree_Model = res)
+table( kmeans_model = k_cl$cluster[600:699], Tree_Model = res)
+
+
+
 #Note that instead of having 2 and 4 we have 1 and 2 indicating these
 #two categorical values.
 #We can tell that the model misses only couple of cells.
@@ -105,13 +107,13 @@ table(2*as.numeric(dat[600:699,11]),res1)
 ############################################################
 #Regression Model
 #Takes into consideration all domains - whole data
-modelreg=lm(Class~dat$Mitoses +dat$Uniformity_of_Cell_Size + dat$Bare_Nuclei +dat$Clump_Thickness)
-modelreg=lm(datClass~.,data=dat)
+#modelreg=lm(as.numeric(Class)~.,data=dat[,2:10])
+modelreg=lm(datClass~.,x=dat[1:599,2:10])
 summary(modelreg)
 
 #It is ok if included data.frame(x=X_new) - X_new extra values for prediction by model.
 #In essence p are the predicted values for the dat input
-p=predict(modelreg,  data.frame(x = dat))
+p=predict(modelreg,  x = dat[600:699,2:10])
 
 #Processing the p float value in order to make it being able to take 2 categorical values
 p1 = c()
